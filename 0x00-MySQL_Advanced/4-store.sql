78f4994d31d9b8912a3ddc7791b87f8f4fb0a736
netia-melvin <melvinatemba18@gmail.com>
2024-10-17 05:44:18 +0000

    fix

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,15 +1,11 @@
--- Initial
-DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS orders;
-
-CREATE TABLE IF NOT EXISTS items (
-    name VARCHAR(255) NOT NULL,
-    quantity int NOT NULL DEFAULT 10
-);
-
-CREATE TABLE IF NOT EXISTS orders (
-    item_name VARCHAR(255) NOT NULL,
-    number int NOT NULL
-);
-
-INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER $$
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
