4aff9d8ac4a0ee961b1b92c8561be934f328ffdc
netia-melvin <melvinatemba18@gmail.com>
2024-10-17 05:49:33 +0000

    store

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,11 +1,4 @@
-DROP TRIGGER IF EXISTS reduce_quantity;
-DELIMITER $$
-CREATE TRIGGER reduce_quantity
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-        SET quantity = quantity - NEW.number
-        WHERE name = NEW.item_name;
-END $$
-DELIMITER ;
+-- Script that creates a trigger that decreses the
+-- quantity of an item after adding a new order
+CREATE TRIGGER decrease_q AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
