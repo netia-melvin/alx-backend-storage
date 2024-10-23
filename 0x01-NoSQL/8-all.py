01401096bbc00323701afac0afaf2f1639d9db97
netia-melvin <melvinatemba18@gmail.com>
2024-10-23 10:53:04 +0000

    python function fix

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,6 +3,7 @@
 Write a Python function that lists all documents in a collection:
 '''
 def list_all(mongo_collection):
-
-    return [doc for doc in mongo_collection.find()]
+    """Lists all docs"""
+    documents = mongo_collection.find()
+    return documents
 
