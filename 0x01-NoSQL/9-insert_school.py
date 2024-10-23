85711984e11c008703dc9004ebd546e4aab9f627
netia-melvin <melvinatemba18@gmail.com>
2024-10-23 11:05:28 +0000

    insert function

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
 def insert_school(mongo_collection, **kwargs):
+    """inserts a new document"""
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
